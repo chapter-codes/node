0654f93ee007428e2c24e9927677ba59b52e03ff
churchill nnanna <mrxyfactor@gmail.com>
2025-03-22 19:22:12 -0700

    instaleed dependencies

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const Event = require('./myEvent.js')
 Event.emit('log')
 
+const express= require('express')
+const app= express()
+// console.log(app)
+app.get('/' , (req, res)=>{console.log('done'); res.send('hit endpoint')})
+app.listen('3000', ()=>console.log('listening'))
